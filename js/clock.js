36a47815f6d35c5c7cd5637fb03adf52b6817ac1
Asroyx <mystoganza@hotmail.com>
2023-10-10 13:51:31 +0300

    feat: Add clock that displays day and time in English

--- js/clock.js
+++ js/clock.js
@@ -2,3 +2,22 @@ let userInputName = prompt("What is your name?");
 
 let greeting = document.querySelector("#myName");
 greeting.innerHTML = userInputName;
+
+function showTime() {
+  const clockElement = document.getElementById("myClock");
+
+  setInterval(function () {
+    const date = new Date();
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const day = date.toLocaleDateString("en-US", { weekday: "long" });
+
+    const time = `${hours}:${minutes}:${seconds}`;
+
+    clockElement.innerHTML = time + " " + day;
+  }, 1000);
+}
+
+document.addEventListener("DOMContentLoaded", showTime);
